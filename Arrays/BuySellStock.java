9e6b83bb91d3199b637edf0cee725d98d1ff9d11
gauravsharma5722 <s.gaurav5722@gmail.com>
2026-02-03 22:04:34 +0530

    BuySellStock

---
+++ Arrays/BuySellStock.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class BuySellStock {
+
+
+    public static int maxProfit(int[] prices) {
+        // Code here
+       int min=Integer.MAX_VALUE,minin=0,max=Integer.MIN_VALUE,maxin=0;
+        int n=prices.length;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(prices[i]<min){
+                min=prices[i];
+                minin=i;
+            }
+            if(maxin <minin)
+            {
+                max=0;
+            }
+            if(prices[i]>max){
+                max=prices[i];
+                maxin=i;
+            }
+            if(min<max && minin<maxin)
+            {
+            
+            ans=Math.max(max-min,ans);
+            }
+            
+        }
+       
+        
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+    Scanner sc=new Scanner(System.in);
+        String[] s=sc.nextLine().split(" ");
+        int n=s.length;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=Integer.parseInt(s[i]);
+        }
+
+
+        System.out.println("Maximum Profit is: " + maxProfit(arr));
+    
+    }
+
+}
